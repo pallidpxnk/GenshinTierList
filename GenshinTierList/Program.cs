caaffc581605211f35238da8dee883abc02d6814
pallidpxnk <k.holovanchuk@icloud.com>
2022-11-10 19:20:31 +0200

    add login and registration pages

--- GenshinTierList/Program.cs
+++ GenshinTierList/Program.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GenshinTierList.Areas.Identity.Data;
 using Microsoft.Extensions.DependencyInjection;
 using AspNetCoreWebApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<GenshinTierListContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GenshinTierListContext") ?? throw new InvalidOperationException("Connection string 'GenshinTierListContext' not found.")));
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,11 +32,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
